2789909a82fdc4fcbc66b4858fbf3fb12ff1f4e1
djantzen <mr.djantzen@gmail.com>
2012-04-10 20:34:21 -0700

    obsolete

--- app/models/unit.rb
+++
@@ -1,5 +0,0 @@
-class Unit < ActiveRecord::Base
-  has_many :resistance_measurements, :class_name => 'Measurement', :foreign_key => 'resistance_unit_id'
-  has_many :distance_measurements, :class_name => 'Measurement', :foreign_key => 'distance_unit_id'
-  has_many :pace_measurements, :class_name => 'Measurement', :foreign_key => 'pace_unit_id'
-end
