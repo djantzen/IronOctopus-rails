090c641f9aa5178b416621dc451388e757e3d423
djantzen <mr.djantzen@gmail.com>
2012-11-14 21:14:33 -0800

    exception handling around stale sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,11 @@ class ApplicationController < ActionController::Base
   private
   
   def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    begin
+      @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    rescue RecordNotFound => rnf
+      session[:user_id] = nil
+    end
   end
 
   def authenticate_user
