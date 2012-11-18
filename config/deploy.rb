9471a854be64ae77d06f42c145306e2a27498b19
djantzen <mr.djantzen@gmail.com>
2012-11-17 16:37:45 -0800

    deploy tasks for bundler and migrate

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,37 +15,50 @@ role :db,  "ironoctop.us", :primary => true # This is where Rails migrations wil
 # these http://github.com/rails/irs_process_scripts
 
 # If you are using Passenger mod_rails uncomment this:
-namespace :deploy do
-   task :start do ; end
-   task :stop do ; end
-   task :restart, :roles => :app, :except => { :no_release => true } do
-     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-   end
-end
 
-def sed(working_directory, file, target, replacement)
-  run "cd #{working_directory} && sed -i 's|password: #{target}|password: #{replacement}|' #{file}"
+def sed(file, target, replacement)
+  run "cd #{release_path} && sed -i 's|password: #{target}|password: #{replacement}|' #{file}"
 end
 
 namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+
   desc "Set email password"
   task :set_email_password, :roles => :app do
-    set(:email_password, Capistrano::CLI.ui.ask("Email password: ") { |q| q.echo = false })
-    sed('/var/www/IronOctopus', 'config/email.yml', 'EMAIL_PASSWORD', fetch(:email_password))
+    set(:email_password, Capistrano::CLI.ui.ask("Email password: "))
+    sed('config/email.yml', 'EMAIL_PASSWORD', fetch(:email_password))
   end
 
   desc "Set database passwords"
   task :set_database_passwords, :roles => :app do
-    set(:admin_password, Capistrano::CLI.ui.ask("Administrator password: ") { |q| q.echo = false })
-    set(:application_password, Capistrano::CLI.ui.ask("Application password: ") { |q| q.echo = false })
-    sed('/var/www/IronOctopus', 'config/database.yml', 'ADMIN_PASSWORD', fetch(:admin_password))
-    sed('/var/www/IronOctopus', 'config/database.yml', 'APPLICATION_PASSWORD', fetch(:application_password))
+    set(:admin_password, Capistrano::CLI.ui.ask("Administrator password: "))
+    set(:application_password, Capistrano::CLI.ui.ask("Application password: "))
+    sed('config/database.yml', 'ADMIN_PASSWORD', fetch(:admin_password))
+    sed('config/database.yml', 'APPLICATION_PASSWORD', fetch(:application_password))
+  end
+
+  desc "Run migrations with admin permissions"
+  task :migrate_production, :roles => :app do
+    set(:rails_env, "migrate_production")
+    migrate
+    set(:rails_env, "production")
   end
 end
 
-after "deploy:update_code", "deploy:set_database_passwords"
-after "deploy:set_database_password" do
-  ENV['RAILS_ENV']="migrate_production"
-  :migrate
+namespace :bundle do
+
+  desc "run bundle install and ensure all gem requirements are met"
+  task :install do
+    run "cd #{release_path} && sudo bundle install"
+  end
+
 end
+
+after "deploy:update_code", "bundle:install"
+after "deploy:update_code", "deploy:set_database_passwords"
+after "deploy:set_database_passwords", "deploy:migrate_production"
 after "deploy:update_code", "deploy:set_email_password"
