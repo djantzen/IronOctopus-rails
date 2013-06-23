6afc0fd8fe8a1309038b7702637395e11187522e
djantzen <mr.djantzen@gmail.com>
2013-06-22 19:19:07 -0700

    delete a bunch of empty tests

--- test/functional/programs_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class ProgramsControllerTest < ActionController::TestCase
-
-
-
-end
--- test/functional/tour_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class TourControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
